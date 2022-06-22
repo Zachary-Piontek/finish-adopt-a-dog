45ba7e77faf953557d7ff91b0a3bd5575a19786a
Zachary Piontek <fortunefavorsthebrave@protonmail.com>
2022-06-21 18:46:31 -0700

    added template literal with expression for img and intro

--- components/DogDetail.js
+++ components/DogDetail.js
@@ -7,10 +7,10 @@ export default function createDogDetail(root) {
 
     return ({ dog }) => {
         // *** use the dog data to complete the following:
-        img.src = dog.img; // get the image based on breed from folder ../assets/dogs/
+        img.src = `../assets/dogs/${dog.breed}.jpeg`; // get the image based on breed from folder ../assets/dogs/
         img.alt = dog.name; // use the dog's name as the alt
         // format a string like "Googoo is a 2 year old beagle":
-        intro.textContent = dog.intro;
+        intro.textContent = `${dog.name} is a ${dog.age} year old ${dog.breed}`;
         description.textContent = dog.description; // dog description
     };
 }
